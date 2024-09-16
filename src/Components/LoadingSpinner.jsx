e442a1f7989c2a01b23e51f25dbd72d5e5a588b5
Gab2903 <gabriel-koeppen@gmx.de>
2024-09-16 16:12:31 +0200

    loading spinner

---
+++ src/Components/LoadingSpinner.jsx
@@ -0,0 +1,9 @@
+const LoadingSpinner = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="animate-spin rounded-full h16 w16 border-t-4 border-slate-500 border-solid"></div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
